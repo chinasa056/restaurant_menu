fa178ee7224b0af76654d73cfc4f98759ac70314
chinasa056 <chinasaacha05@gmail.com>
2025-01-26 19:41:06 +0100

    made changes to router end points

--- router/ordersRouter.js
+++ router/ordersRouter.js
@@ -2,7 +2,7 @@ const { createOrder, getAllOrders, getAllOrdersByMenu, updateOrder, deleteOrder 
 
 const router = require("express").Router();
 
-router.post("/order", createOrder)
+router.post("/order/:menuId", createOrder)
 router.get("/order", getAllOrders)
 router.get("/order/:id", getAllOrdersByMenu)
 router.put("/order/:id", updateOrder)
