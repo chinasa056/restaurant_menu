fe5b3e707ddab3a20aba4ee66d0586aaeaafd76f
chinasa056 <chinasaacha05@gmail.com>
2025-01-26 19:25:41 +0100

    made changes to the migrate and models

--- migrations/20250125124010-create-menu-items.js
+++ migrations/20250125124010-create-menu-items.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('menu_Items', {
+    await queryInterface.createTable('menu_items', {
       id: {
         allowNull: false,
         primaryKey: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Menu_Items');
+    await queryInterface.dropTable('menu_items');
   }
 };
--- migrations/20250125125516-create-orders.js
+++ migrations/20250125125516-create-orders.js
@@ -41,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Orders');
+    await queryInterface.dropTable('orders');
   }
 };
