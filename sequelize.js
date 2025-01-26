85a27be2a9c38beb881e46bcf7a7c37bd57aa31e
chinasa056 <chinasaacha05@gmail.com>
2025-01-26 11:55:21 +0100

    i updated the config

--- sequelize.js
+++ sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('Restaurant_Menu', 'root', 'Acha105#', {
-    host: 'localhost',
+const sequelize = new Sequelize('sql12759428', 'sql12759428', 'jys97Y8nhl', {
+    host: 'sql12.freesqldatabase.com',
     dialect: "mysql"
   });
 
